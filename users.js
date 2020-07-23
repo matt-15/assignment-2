a3e10c4ac12784776763226c8a09c542736ed067
Cassandra-Fu <68042895+Cassandra-Fu@users.noreply.github.com>
2020-07-23 23:06:57 +0800

    fixed XSS in this file

---
+++ users.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $(".eclectic-deactivate-btn").click(function () {
+    swal({
+      title: "Are you sure?",
+      text: "Once deactivated, user will no longer be able to login",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    })
+    .then((willclose) => {
+      if (willclose) {
+        const usr_id = parseInt(this.value);
+        postData("/api-service/user/deactivate/", {"id":usr_id})
+          .then((data) => {
+            if (data.success === "true") {
+              swal({
+                title: "Account deactivated",
+                icon: "success",
+              })
+                .then(() => {
+                  window.location.href = encodeURIComponent("/dashboard/users/");
+                });
+            }
+          });
+      } else {
+        swal({
+          title: "Account will not be deactivated",
+          icon: "info"
+        });
+      }
+    });
+  });
+});
