69443a36f6c4bdc4dcc62a752229b8bcc9a79fe9
Cassandra-Fu <68042895+Cassandra-Fu@users.noreply.github.com>
2020-07-23 23:09:57 +0800

    Fixed XSS in this file

--- static/js/orders.js
+++ static/js/orders.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   $("#eclectic-deliver-btn").click(function () {
-    window.location.href = "/dashboard/orders/?delivered=false";
+    window.location.href = encodeURIComponent("/dashboard/orders/?delivered=false");
   });
   $("#eclectic-history-btn").click(function () {
-    window.location.href = "/dashboard/orders/?delivered=true";
+    window.location.href = encodeURIComponent("/dashboard/orders/?delivered=true");
   });
   $("#eclectic-all-btn").click(function () {
-    window.location.href = "/dashboard/orders/";
+    window.location.href = encodeURIComponent("/dashboard/orders/");
   });
 });
